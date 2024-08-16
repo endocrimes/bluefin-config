12010df1bbf4a814421508e3cc1cbf14f3ddc124
Danielle Lancashire <dani@builds.terrible.systems>
2024-08-16 21:26:53 +0200

    install 1password and better vim, rebase on dx

--- Containerfile
+++ Containerfile
@@ -7,7 +7,7 @@
 ARG SOURCE_IMAGE="bluefin"
 
 ## SOURCE_SUFFIX arg should include a hyphen and the appropriate suffix name
-ARG SOURCE_SUFFIX=""
+ARG SOURCE_SUFFIX="-dx"
 
 ## SOURCE_TAG arg must be a version built for the specific image: eg, 39, 40, gts, latest
 ARG SOURCE_TAG="40"
@@ -22,7 +22,9 @@ FROM ghcr.io/ublue-os/${SOURCE_IMAGE}${SOURCE_SUFFIX}:${SOURCE_TAG}
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
 
+
 COPY build.sh /tmp/build.sh
+COPY --chmod=0755 scripts/* /tmp/
 
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
--- build.sh
+++ build.sh
@@ -4,20 +4,30 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+### Remove packages
+
+rpm-ostree override remove \
+  code
 
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/40/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install \
+  kitty \
+  clang \
+  ninja-build \
+  vim-X11
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+### add 1password
+chmod +x /tmp/1password.sh && \
+  ONEPASSWORD_RELEASE_CHANNEL=beta \
+  GID_ONEPASSWORD=1500 \
+  GID_ONEPASSWORDCLI=1600 \
+  /tmp/1password.sh
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
