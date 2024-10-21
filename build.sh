2d0814f2debeb864cd1a139498b5916dcff9cfac
Danielle <dani@builds.terrible.systems>
2024-10-21 20:40:02 +0200

    1password: switch back to beta

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ rpm-ostree install \
 
 ### add 1password
 chmod +x /tmp/1password.sh && \
-  ONEPASSWORD_RELEASE_CHANNEL=stable \
+  ONEPASSWORD_RELEASE_CHANNEL=beta \
   GID_ONEPASSWORD=1500 \
   GID_ONEPASSWORDCLI=1600 \
   /tmp/1password.sh
